980decf555f6202461286d9965db256d2fdd0614
Dmitry Kholodilov <dmitry.kholodilov@gmail.com>
2014-04-17 00:29:14 +0400

    added task03 files

---
+++ task03/src/dsl/core.clj
@@ -0,0 +1,32 @@
+(ns dsl.core
+  (:use clojure.walk))
+
+(def cal (java.util.Calendar/getInstance))
+(def today (java.util.Date.))
+(def yesterday (do (.add cal java.util.Calendar/DATE -1) (.getTime cal)))
+(def tomorrow (do (.add cal java.util.Calendar/DATE 2) (.getTime cal)))
+
+(comment
+  (defn one [] 1)
+  
+  ;; Примеры вызова
+  (with-datetime
+    (if (> today tomorrow) (println "Time goes wrong"))
+    (if (<= yesterday today) (println "Correct"))
+    (let [six (+ 1 2 3)
+          d1 (today - 2 days)
+          d2 (today + 1 week)
+          d3 (today + six months)
+          d4 (today + (one) year)]
+      (if (and (< d1 d2)
+               (< d2 d3)
+               (< d3 d4))
+        (println "DSL works correctly")))))
+
+
+;; Режим Бога -- никаких подсказок.
+;; Вы его сами выбрали ;-)
+(defmacro with-datetime [& code]
+  :ImplementMe!)
+
+
