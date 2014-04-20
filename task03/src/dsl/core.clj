3935e8ab5a5d6326092114b505239a89e17f3f04
Dmitry Kholodilov <dmitry.kholodilov@gmail.com>
2014-04-20 11:18:37 +0400

    task03 - macro implementation passing all tests

--- task03/src/dsl/core.clj
+++ task03/src/dsl/core.clj
@@ -1,5 +1,8 @@
 (ns dsl.core
-  (:use clojure.walk))
+  (:use [clojure.walk :only (prewalk)])
+  (:use [clojure.core.match :only (match)])
+  ;(:import (org.apache.commons.lang3.time DateUtils))
+  )
 
 (def cal (java.util.Calendar/getInstance))
 (def today (java.util.Date.))
@@ -8,7 +11,7 @@
 
 (comment
   (defn one [] 1)
-  
+
   ;; Примеры вызова
   (with-datetime
     (if (> today tomorrow) (println "Time goes wrong"))
@@ -21,32 +24,38 @@
       (if (and (< d1 d2)
                (< d2 d3)
                (< d3 d4))
-        (println "DSL works correctly")))))
+        (println "DSL works correctly"))))
+ )
 
+(def date-symbols
+  {
+    'day     'org.apache.commons.lang3.time.DateUtils/addDays
+    'days    'org.apache.commons.lang3.time.DateUtils/addDays
+    'week    'org.apache.commons.lang3.time.DateUtils/addWeeks
+    'weeks   'org.apache.commons.lang3.time.DateUtils/addWeeks
+    'month   'org.apache.commons.lang3.time.DateUtils/addMonths
+    'months  'org.apache.commons.lang3.time.DateUtils/addMonths
+    'year    'org.apache.commons.lang3.time.DateUtils/addYears
+    'years   'org.apache.commons.lang3.time.DateUtils/addYears
+  })
 
-;; Режим Бога -- никаких подсказок.
-;; Вы его сами выбрали ;-)
+(defn date-symbol? [sym] (and (symbol? sym) (contains? date-symbols sym)))
+
 (defmacro with-datetime [& code]
+  (let [transformed-code
+         (prewalk
+           (fn [form]
+             (if (list? form)
+               (match (vec form)
+                 [date op n (sym :guard date-symbol?)]
+                   (let [transform-fn (sym date-symbols)
+                         op-str (str op)]
+                     `(~transform-fn ~date (Integer/parseInt (str ~op-str ~n))))
+                 :else form)
+               form))
+           code)]
   `(let [~(symbol ">") (fn [d1# d2#] (> (.compareTo d1# d2#) 0))
          ~(symbol "<") (fn [d1# d2#] (< (.compareTo d1# d2#) 0))
          ~(symbol "<=") (fn [d1# d2#] (<= (.compareTo d1# d2#) 0))
-         ~(symbol ">=") (fn [d1# d2#] (>= (.compareTo d1# d2#) 0))
-         ~(symbol "day") '()
-         ~(symbol "days") '()
-         ~(symbol "week") '()
-         ~(symbol "weeks") '()
-         ~(symbol "month") '()
-         ~(symbol "months") '()
-         ~(symbol "year") '()
-         ~(symbol "years") '()]
-      ~@code))
-
-(comment
-  (macroexpand-1
-    '(with-datetime
-      (if (> today tomorrow) (println "Time goes wrong") (println "ok"))))
-
-  (with-datetime
-    (if (> today tomorrow) (println "Time goes wrong") (println "Correct"))
-    (if (<= yesterday today) (println "Correct") (println "Time goes wrong")))
-)
+         ~(symbol ">=") (fn [d1# d2#] (>= (.compareTo d1# d2#) 0))]
+      ~@transformed-code)))
